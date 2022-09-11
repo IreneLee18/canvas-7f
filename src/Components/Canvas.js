b0c1ee6a65d60699aaa6a894a6193c5ede765ecf
IreneLee18 <cc86418520@gmail.com>
2022-09-11 18:54:16 +0800

    fix undo redo clearAll

--- src/Components/Canvas.js
+++ src/Components/Canvas.js
@@ -1,29 +1,29 @@
 import { useOnDraw } from "../Hooks/CanvasHooks";
-import { useRef, useState, useLayoutEffect, useImperativeHandle, forwardRef } from "react";
+import { useRef, useState, useCallback, useLayoutEffect, useImperativeHandle, forwardRef } from "react";
 
 function Canvas( { props, backgroundColor, color, size, setHaveUndo, setHaveRedo }, ref ) {
   const currentSize = useRef(size);
   const currentColor = useRef(color);
-  const currentDraw = useRef();
-  const undoListData = useRef([]);
-  const [undoList, setUndoList] = useState(null);
-  const redoListData = useRef([]);
-  const [redoList, setRedoList] = useState(null);
-  const { setCanvasRef, startPainting, ctx, isPainting, canvasRef } = useOnDraw(onDraw);
+  const pic = useRef(new Image()); // 建立新的 Image
+  const step = useRef(0);
+  const canvasData = useRef([]);
+  const [canvasArray, setCanvasArray] = useState(null);
+  const { setCanvasRef, startPainting, ctx, isPainting, canvasRef } =
+    useOnDraw(onDraw);
+
+  useLayoutEffect(() => {
+    if (canvasData.current.length === 0) {
+      init();
+    }
+  }, []);
+
   useLayoutEffect(() => {
     currentColor.current = color;
     currentSize.current = size;
-    // console.log("un", undoList, "re", redoList);
-    // console.log(undoListData.current, redoListData.current);
-    if (redoListData.current.length === 0) {
-      setHaveRedo(false);
-    }
-    if (undoListData.current.length === 0) {
-      setHaveUndo(false);
-    }
-  }, [color, currentColor, size, undoList, redoList, setHaveRedo, setHaveUndo]);
+  }, [color, size]);
 
   function onDraw(ctx, x, y) {
+    if (!isPainting) return;
     ctx.lineWidth = Number(currentSize.current);
     ctx.lineCap = "round";
     ctx.strokeStyle = currentColor.current;
@@ -32,27 +32,61 @@ function Canvas( { props, backgroundColor, color, size, setHaveUndo, setHaveRedo
     ctx.beginPath();
     ctx.moveTo(x, y);
   }
+
   const stopPainting = () => {
+    if (!isPainting.current) return;
+    if (step.current !== canvasData.current.length - 1) {
+      canvasData.current = canvasData.current.splice(0, step.current + 1);
+    }
+    step.current++;
     isPainting.current = false;
     ctx.current.beginPath();
-    currentDraw.current = ctx.current.getImageData( 0, 0, window.outerWidth, window.outerHeight );
-    undoListData.current.push(currentDraw.current);
+    canvasData.current.push(canvasRef.current.toDataURL());
+    getImage();
     setHaveUndo(true);
-    setUndoList(() => [...undoListData.current]);
+    setHaveRedo(false);
   };
+
   const init = () => {
     setHaveUndo(false);
     setHaveRedo(false);
-    setUndoList(null);
-    setRedoList(null);
-    undoListData.current = [];
-    redoListData.current = [];
+    if (canvasData.current.length > 1) {
+      canvasData.current = canvasData.current.splice(0, 1);
+      step.current = 0;
+      getImage();
+    } else if (canvasData.current.length === 0) {
+      canvasData.current.push(canvasRef.current.toDataURL());
+      setCanvasArray(canvasData.current[0]);
+      pic.current.src = canvasArray;
+    }
   };
+
+  const handleCanvasClick = () => {
+    if (props.id !== "fill") return;
+    fill(currentColor.current);
+  };
+
   const fill = (color) => {
     ctx.current.fillStyle = color;
     ctx.current.rect(0, 0, window.outerWidth, window.outerHeight);
     ctx.current.fill();
   };
+
+  const getImage = useCallback(() => {
+    setCanvasArray(() => canvasData.current[step.current]);
+    pic.current.src = canvasArray; //載入影像
+    pic.current.onload = function () {
+      ctx.current.clearRect(0, 0, window.outerWidth, window.outerHeight);
+      ctx.current.drawImage(pic.current, 0, 0);
+    };
+  }, [canvasArray, ctx]);
+
+  useLayoutEffect(() => {
+    getImage();
+    if (step.current === 0) setHaveUndo(false);
+    if (step.current === canvasData.current.length - 1) setHaveRedo(false);
+  }, [getImage, setHaveRedo, setHaveUndo]);
+
   useImperativeHandle(ref, () => ({
     saveCanvas: () => {
       document.getElementById("save").href = canvasRef.current.toDataURL();
@@ -62,33 +96,21 @@ function Canvas( { props, backgroundColor, color, size, setHaveUndo, setHaveRedo
       init();
     },
     undoFunc: () => {
-      ctx.current.putImageData( undoListData.current[undoListData.current.length - 1], 0, 0);
-      setHaveRedo(true);
-      // 取得目前畫筆 ＝ 最後一個的畫筆
-      currentDraw.current = undoListData.current.pop();
-      // 下一個畫筆 ＝ 目前畫筆
-      redoListData.current.unshift(currentDraw.current);
-      // 刪除上一筆
-      setUndoList(() => [...undoListData.current]);
-      // 新增下一筆
-      setRedoList(() => [...redoListData.current]);
+      if (step.current > 0) {
+        step.current--;
+        getImage();
+        setHaveRedo(true);
+      }
     },
     redoFunc: () => {
-      ctx.current.putImageData( redoListData.current[redoListData.current.length - 1], 0, 0);
-      // 取得目前畫筆 ＝ 最後一個的畫筆
-      currentDraw.current = redoListData.current.shift();
-      // 下一個畫筆 ＝ 目前畫筆
-      undoListData.current.push(currentDraw.current);
-      // 刪除下一筆
-      setRedoList(() => [...redoListData.current]);
-      // 新增上一筆
-      setUndoList(() => [...undoListData.current]);
+      if (step.current < canvasData.current.length - 1) {
+        step.current++;
+        getImage();
+        setHaveUndo(true);
+      }
     },
   }));
-  const handleCanvasClick = () => {
-    if (props.id !== "fill") return;
-    fill(currentColor.current);
-  };
+
   return (
     <>
       <canvas
@@ -99,7 +121,6 @@ function Canvas( { props, backgroundColor, color, size, setHaveUndo, setHaveRedo
         onClick={handleCanvasClick}
         onMouseDown={startPainting}
         onMouseUp={stopPainting}
-        // onMouseMove={painting}
       ></canvas>
     </>
   );
